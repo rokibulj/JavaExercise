30b62b5ca203d0132f2e207e732f9472831bb515
Md Rokibul Hassan <rokibulj2@gmail.com>
2016-12-03 22:51:20 -0500

    organized class

--- src/day/one/practice/MethodOne.java
+++ src/day/one/practice/MethodOne.java
@@ -1,20 +1,18 @@
 package day.one.practice;
 
 public class MethodOne {
-	
-	public static void main(String[] args){
-		
+
+	public static void main(String[] args) {
+
 		System.out.println(getName("Md Rokibul Hassan"));
-		
-		
+
 	}
-	
-	public static String getName(String name){
+
+	public static String getName(String name) {
 		String sameName = "";
-		
+
 		sameName = name;
-		
-		
+
 		return sameName;
 	}
 
