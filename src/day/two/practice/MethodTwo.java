91b6829683236c04d6de19e548b3c19cfdeac005
Md Rokibul Hassan <rokibulj2@gmail.com>
2016-12-03 23:12:20 -0500

    Method Two add

---
+++ src/day/two/practice/MethodTwo.java
@@ -0,0 +1,32 @@
+package day.two.practice;
+
+import java.util.ArrayList;
+
+public class MethodTwo {
+
+	public static void main(String[] args) {
+
+		System.out.println(getEvenList(21));
+
+	}
+
+	/**
+	 * Write a method that take one number input and return the list of all even
+	 * number from 0 to that given number.
+	 */
+
+	public static ArrayList<Integer> getEvenList(int number) {
+		ArrayList<Integer> evenList = new ArrayList<Integer>();
+
+		for (int i = 0; i <= number; i++) {
+
+			if (i % 2 == 0) {
+
+				evenList.add(i);
+			}
+		}
+
+		return evenList;
+	}
+
+}
